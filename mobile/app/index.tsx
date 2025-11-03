61294bf90461eb9b04a7442c2936f29676ab4cbc
Eva-M-Lopez <evaml@Evas-Computer.localdomain>
2025-11-03 12:59:14 -0500

    Revert "Merge pull request #6 from Eva-M-Lopez/andrew-mobile-fix"
    This reverts commit 7716ccaf93b631c9555ddf124ae44b6b1d4641d1, reversing
    changes made to 76779f2cc30fb695562f3b0ab9b4a525bef13ea6.

--- mobile/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // Redirect root to login screen
-  return <Redirect href="/login" />;
-}
--- shared/src/types/index.ts
+++ shared/src/types/index.ts
@@ -1,16 +1,1 @@
-export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
-
-export interface LoginRequest {
-  login: string;
-  password: string;
-}
-
-export interface LoginResponse {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
+export {};
